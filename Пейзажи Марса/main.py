3a7d9bdc96cc3f08b0be125d20db432e0d8e2163
Komarik230 <105167716+Komarik230@users.noreply.github.com>
2023-04-28 21:20:40 +0300

    Задача "Пейзажи Марса"

--- Пейзажи Марса/main.py
+++ Пейзажи Марса/main.py
@@ -2,51 +2,54 @@ from flask import Flask, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/carousel')
 def carousel():
     return f'''<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Пейзажи Марса</title>
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crossorigin="anonymous">
 
-                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                        <title>Пейзажи Марса</title>
-                      </head>
-                      <body>
-                        <h1 class='text'>Пейзажи Марса</h1>
-                        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                          <div class="carousel-inner">
-                            <div class="carousel-item active image">
-                              <img src="static/img/i.png" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item image">
-                              <img src="static/img/i.jpg" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item image">
-                              <img src="static/img/i(2).jpg" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item image">
-                              <img src="static/img/i(3).jpg" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item image">
-                              <img src="static/img/i(4).jpg" class="d-block w-100" alt="...">
-                            </div>
-                          </div>
-                          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                          </button>
-                          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                          </button>
-                        </div>
-                      </body>
-                    </html>'''
+    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}"/>
+</head>
+<body>
+<h1>Пейзажи Марса</h1>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="{url_for('static', filename='img/mars1.jpg')}" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="{url_for('static', filename='img/mars2.jpg')}" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="{url_for('static', filename='img/mars3.jpg')}" alt="Third slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>'''
 
 
 if __name__ == '__main__':
